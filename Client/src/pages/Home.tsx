e9c7f9318e1c201c6b40b495433788d11c944fda
unknown <antoinecarrette@hotmail.fr>
2026-05-25 15:14:18 +0200

    build: typing on Home page on 25/05/2026

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,4 +1,17 @@
 export const Home = () => {
+
+type SecurityCheck = {
+  title: string;
+  status: "good" | "warning" | "bad";
+  message: string;
+};
+
+type ScanResult = {
+  score: number;
+  summary: string;
+  checks: SecurityCheck[];
+};
+
   return (
     <div>Home</div>
   )
